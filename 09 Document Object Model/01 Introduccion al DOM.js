4c260e40e84bf18d7e7986c65642992acbf926e3
Martin Moreno <102624788+MorenoMartinE@users.noreply.github.com>
2022-05-05 22:12:19 -0300

    Add files via upload

---
+++ 09 Document Object Model/01 Introduccion al DOM.js
@@ -0,0 +1,17 @@
+//      Documentación:
+// https://developer.mozilla.org/es/docs/Glossary/DOM
+
+
+//      La forma de acceder a los elementos del DOM, es a travez del uso de el objeto document
+console.log(document);         // Con el objeto document podemos acceder a todos los elementos pertenecientes al DOM
+console.log(document.head);    // Y con la notacion de punto podremos acceder a los elementos de manera individual
+
+
+//      Documentación:
+// https://developer.mozilla.org/es/docs/Web/API/Node/nodeType 
+// Ignorar nodos xml, los mas importantes seran los nodos de tipo texto y los de tipo elemento.
+// https://developer.mozilla.org/es/docs/Web/API/Document
+//      IMPORTANTE: Todos los metodos selectores devuelven Nodelists, a pesar de que estos comparten muchas
+// cualidades de los arreglos. Estos no lo son.
+// https://developer.mozilla.org/es/docs/Web/API/NodeList
+
